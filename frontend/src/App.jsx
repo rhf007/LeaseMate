6bfc7dd63ccd301745d53904502a1ec5937ae12d
Nadeen ElGendi <nadineamrelgendi@gmail.com>
2025-06-30 13:43:25 +0300

    setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold">Hello World</h1>
+        <p className="mt-4 text-gray-600">Welcome to my Tailwind CSS + DaisyUI app!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
