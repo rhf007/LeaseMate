8bef66a1aa3b4d8b1836699cafbf75d996e8de62
rhf007 <rahofahazem512@gmail.com>
2025-07-06 21:27:07 +0300

    unit model

--- backend/models/unit.model.js
+++ backend/models/unit.model.js
@@ -1,1 +1,41 @@
 const mongoose = require("mongoose");
+
+const unitSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    isFurnished: {
+        type: Boolean,
+        required: true
+    },
+    numRooms: {
+        type: Number,
+        required: true
+    },
+    space: {
+        type: Number,
+        required: true
+    },
+    address:{
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    governorate: {
+        type: String,
+        required: true
+    },
+    postalCode: {
+        type: Number
+    },
+})
+
+module.exports = mongoose.model("Units", unitSchema)
+//TODO: images, proprtyid, price, ownerid, amenities, available dates,  
