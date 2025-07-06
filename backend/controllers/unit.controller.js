21bfaa7f6943e4d352ef9730d6b15ece7ab625eb
rhf007 <rahofahazem512@gmail.com>
2025-07-06 22:23:48 +0300

    get units

--- backend/controllers/unit.controller.js
+++ backend/controllers/unit.controller.js
@@ -1,1 +1,11 @@
-const Unit = require("../models/unit.model")
+const asyncWrapper = require("../middlewares/asyncWrapper");
+const Unit = require("../models/unit.model");
+const httpStatusText = require("../utils/httpStatusText");
+const appError = require("../utils/appError");
+
+const getAllUnits = asyncWrapper(async (req, res) => {
+  const units = await Unit.find();
+  res.json({ status: httpStatusText.SUCCESS, data: { units } });
+});
+
+module.exports = { getAllUnits };
